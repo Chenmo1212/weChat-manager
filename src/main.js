564dffc3aa132937edd5b1847b3f6ea6cef573fd
chenmo1212 <160594753@qq.com>
2020-02-08 19:02:06 +0800

    配置element和scss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui';
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
 
 /* eslint-disable no-new */
 new Vue({
